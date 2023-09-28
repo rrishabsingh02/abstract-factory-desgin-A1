815e78e245c23dcaa8f411782b7ec1fe584fa643
rrishabsingh02 <rishab.somvanshi@gmail.com>
2023-09-27 20:50:54 -0400

    file structure

--- src/Apple.java
+++ src/Apple.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Apple implements GroceryProduct {
     @Override
     public String getDescription() {
@@ -6,7 +10,25 @@ public class Apple implements GroceryProduct {
 
     @Override
     public double getPrice() {
-        DataFileReader dataFileReader = new DataFileReader("prices.txt");
-        return dataFileReader.getPrice("Apple");
+        // Read the price from the data file
+        double price = readPriceFromDataFile("prices.txt", "Apple");
+        return price;
+    }
+
+    private double readPriceFromDataFile(String filename, String productName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 2 && parts[0].equals(productName)) {
+                    return Double.parseDouble(parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Default price if not found in the data file
+        return 0.0;
     }
 }
--- src/Orange.java
+++ src/Orange.java
@@ -1,14 +1,35 @@
 package src;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Orange implements GroceryProduct {
     @Override
     public String getDescription() {
-        return "Orange";
+        return "Banana";
     }
 
     @Override
     public double getPrice() {
-        DataFileReader dataFileReader = new DataFileReader("prices.txt");
-        return dataFileReader.getPrice("Orange");
+        // Read the price from the data file
+        double price = readPriceFromDataFile("prices.txt", "Orange");
+        return price;
+    }
+
+    private double readPriceFromDataFile(String filename, String productName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 2 && parts[0].equals(productName)) {
+                    return Double.parseDouble(parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Default price if not found in the data file
+        return 5.0;
     }
 }
