13723a8c903a863bdbb7e2e29f7ed8e2ce2220b9
rrishabsingh02 <rishab.somvanshi@gmail.com>
2023-09-27 20:05:05 -0400

    update to file structure

--- src/Banana.java
+++ src/Banana.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Banana implements GroceryProduct {
     @Override
     public String getDescription() {
@@ -10,25 +6,7 @@ public class Banana implements GroceryProduct {
 
     @Override
     public double getPrice() {
-        // read prices of products from datafile
-        double price = readPriceFromDataFile("prices.txt", "Banana");
-        return price;
-    }
-
-    private double readPriceFromDataFile(String filename, String productName) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                if (parts.length == 2 && parts[0].equals(productName)) {
-                    return Double.parseDouble(parts[1]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // return value if no price found in datafile
-        return 0.0;
+        DataFileReader dataFileReader = new DataFileReader("prices.txt");
+        return dataFileReader.getPrice("Banana");
     }
 }
