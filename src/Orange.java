e072645c0fd99c110644b489cad4e6a77a5dc060
rrishabsingh02 <rishab.somvanshi@gmail.com>
2023-09-27 20:22:17 -0400

    updated file structure

--- src/Orange.java
+++ src/Orange.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+package src;
+
 
 public class Orange implements GroceryProduct {
     @Override
@@ -10,25 +9,7 @@ public class Orange implements GroceryProduct {
 
     @Override
     public double getPrice() {
-        // read prices of products from datafile
-        double price = readPriceFromDataFile("prices.txt", "Orange");
-        return price;
-    }
-
-    private double readPriceFromDataFile(String filename, String productName) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                if (parts.length == 2 && parts[0].equals(productName)) {
-                    return Double.parseDouble(parts[1]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // return value if no price found in datafile
-        return 5.0;
+        DataFileReader dataFileReader = new DataFileReader("prices.txt");
+        return dataFileReader.getPrice("Orange");
     }
 }
